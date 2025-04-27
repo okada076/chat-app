cfc30e15bf717bbec16cd1241f79950875c99e5f
okada076 <416876pom@gmail.com>
2025-04-27 13:33:57 +0900

    結合テストの実装

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,11 +1,7 @@
 FactoryBot.define do
   factory :message do
-    content {Faker::Lorem.sentence}
-    association :user
-    association :room
-
     after(:build) do |message|
-      message.image.attach (io: File.open(image_path), filename: 'test_image.png')
+      message.image.attach(io: File.open(image_path), filename: 'test_image.png')
+    end
   end
-end
 end
